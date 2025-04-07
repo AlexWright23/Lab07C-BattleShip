5f11d5a9640b46980e2211b32fd2724a08ab6247
Alexander Wright <Alexwright45177@gmail.com>
2025-04-07 17:02:19 -0400

    Update BattleshipCell.java

--- src/BattleshipCell.java
+++ src/BattleshipCell.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class BattleshipCell {
+    private int row, col;
+    private boolean clicked;
+    private Ship ship;
+
+    public BattleshipCell(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.clicked = false;
+        this.ship = null;
+    }
+
+    public void click() { clicked = true; }
+    public boolean isClicked() { return clicked; }
+    public boolean hasShip() { return ship != null; }
+    public Ship getShip() { return ship; }
+    public void setShip(Ship ship) { this.ship = ship; }
 }
