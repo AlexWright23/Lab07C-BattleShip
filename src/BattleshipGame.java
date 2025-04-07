75b1fd63ffb803fd5eb28eb634c5575c3a93872e
Alexander Wright <Alexwright45177@gmail.com>
2025-04-07 17:01:31 -0400

    Update BattleshipGame.java

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class BattleshipGame {
+    private BattleshipBoard board;
+    private BattleshipGUI gui;
+
+    public BattleshipGame() {
+        board = new BattleshipBoard();
+        gui = new BattleshipGUI(this, board);
+    }
+
+    public void processClick(int row, int col) {
+        board.fire(row, col);
+        gui.update();
+
+        if (board.allShipsSunk()) {
+            gui.showMessage("You WIN! Play again?");
+        } else if (board.getStrikeCount() >= 3) {
+            gui.showMessage("You LOST! Play again?");
+        }
+    }
+
+    public void resetGame() {
+        board = new BattleshipBoard();
+        gui.reset(board);
+    }
 }
+
+
+
