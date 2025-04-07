793ae393f0a6470d1b51583c84de38206b6f7a6f
Alexander Wright <Alexwright45177@gmail.com>
2025-04-07 17:02:57 -0400

    Update Ship.java

--- src/Ship.java
+++ src/Ship.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Ship {
+    private int size;
+    private int hits;
+    private boolean sunk;
+
+    public Ship(int size) {
+        this.size = size;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    public void hit() {
+        hits++;
+        if (hits >= size) sunk = true;
+    }
+
+    public boolean isSunk() { return sunk; }
+    public void setSunk(boolean sunk) { this.sunk = sunk; }
+    public int getSize() { return size; }
 }
+
